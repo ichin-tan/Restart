f240a3543a9d0611bcd16ff5f194b358609488f6
ichin-tan <chintanpanchal396@gmail.com>
2025-10-28 19:50:08 -0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by CP on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
